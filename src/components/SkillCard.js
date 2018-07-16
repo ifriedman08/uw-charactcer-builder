1f40c781e130be4a4b8995e5b547ec57feb3c8e5
ifriedman08 <i.friedman.08@gmail.com>
2018-07-16 15:26:45 -0700

    styling and icons

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -4,14 +4,15 @@ import _util from '../_util.js';
 class SkillCard extends React.Component {
     render() {
         return (
-            // <div className="container">
-            <div className="card" style={{ "height": "20em", "width": "20em", "marginRight": "1em", "marginBottom": "1em", "display": "inline-block", "verticalAlign": "top"}}>
+            <div className="card text-white bg-dark mb-3 col-lg-4" style={{ "margin": "0.5em" }}>
+              <h5 className="card-header font-weight-bold">
+                <ion-icon name={this.props.skills[this.props.skill].iconName} className="float-right" style={{ "verticalAlign": "top"}}></ion-icon>
+                    {"\u00a0\u00a0" + _util.capitalize(this.props.skill) + "    "}
+              </h5>
               <div className="card-body">
-                  <h6 className="card-title font-weight-bold">{_util.capitalize(this.props.skill)}</h6>
                   <p style={{"whiteSpace": "pre-wrap", "fontSize": "1em"}}>{this.props.skills[this.props.skill].description}</p>
               </div>
             </div>
-          // </div>
         );
     }
 };
