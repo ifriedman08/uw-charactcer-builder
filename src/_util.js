1c3fac6506486046bb60d6290baf7794f837e22d
ifriedman08 <i.friedman.08@gmail.com>
2018-07-24 13:20:58 -0700

    added toggleElementInArray toggleElementInArray to util

--- src/_util.js
+++ src/_util.js
@@ -3,8 +3,32 @@ var capitalize = function (str) {
     return str[0].toUpperCase() + str.slice(1);
 };
 
+var removeElementFromArray = function (el, array) {
+    if (array.length === 1 && array[0] === el) {
+        return []
+    };
+    for (var i = 0; i < array.length - 1; i++) {
+        if (array[i] === el) {
+            array.splice(i, 1);
+            break;
+        }
+    }
+    return array;
+}
+
+var toggleElementInArray = function(el, array){
+    if (!array.includes(el)){
+        array.push(el)
+    } else {
+        return removeElementFromArray(el, array)
+    }
+    return array
+}
+
 var _util = {
-    capitalize
+    capitalize,
+    removeElementFromArray,
+    toggleElementInArray
 }
 
 export default _util;
