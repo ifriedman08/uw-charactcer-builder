6d2b68d4cd7c229fd7987c25e6ef6da6f9fad109
ifriedman08 <i.friedman.08@gmail.com>
2018-07-24 13:19:28 -0700

    conditional switch rendering in skill cards

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -3,14 +3,22 @@ import _util from '../_util.js';
 
 class SkillCard extends Component {
     render() {
+        var skills = this.props.skills;
+        var skill = this.props.skill;
         return (
             <div className="card text-white bg-dark mb-3 col-lg-4" style={{ "margin": "0.5em" }}>
               <h5 className="card-header font-weight-bold">
-                <ion-icon name={this.props.skills[this.props.skill].iconName} className="float-right" style={{ "verticalAlign": "top"}}></ion-icon>
-                    {"\u00a0\u00a0" + _util.capitalize(this.props.skill) + "    "}
+                <ion-icon name={skills[skill].iconName} className="float-right" style={{ "verticalAlign": "top"}}></ion-icon>
+                {"\u00a0\u00a0" + _util.capitalize(skill) + "    "}
+                { this.props.isActiveCareer &&
+                    <span className="switch switch-sm float-right">
+                        <input type="checkbox" className="switch skill-switch" id={`switch-sm-${skill}`} onClick={() => { }} />
+                        <label htmlFor={`switch-sm-${skill}`} style={{ "margin": "0.3em" }}></label>
+                    </span>
+                }
               </h5>
               <div className="card-body">
-                  <p style={{"whiteSpace": "pre-wrap", "fontSize": "1em"}}>{this.props.skills[this.props.skill].description}</p>
+                  <p style={{"whiteSpace": "pre-wrap", "fontSize": "1em"}}>{skills[skill].description}</p>
               </div>
             </div>
         );
